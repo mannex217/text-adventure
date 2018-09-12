f85be1e26685d7a737537502ba1ba7b8901811a1
Merit Student <student@meac.local>
2018-09-12 13:39:46 -0600

    added first stage of development

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("IT'S ALIVE!!!")
+print("To start, type \'start\'")
+fromUser = input()
+if fromUser == "start":
+    print("So let's start")
+else:
+    print("well okay then.............shucks; so what do we do now?")
+fromUser2 = input()
+if fromUser != "lets play a game" or "lets play a game." or "start game":
+    print("well that sounds good and all, but I kinda hoped would could play the game that I spent so many hours making.
+    print(It's not as if I spent many sleepless nights designing the perfect game designed speciffically for ungrateful people like you to play; or something like that")
+    print("but no, IM FINE go ahead and leave; play your platforms and your shooters. I won't mind")
+else:
+    print("Oh, okay then. Let's start then")
